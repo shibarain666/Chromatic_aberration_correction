93cdcdce86cde559cf95d9684dcd0e1957685811
Jerry Cheng <jerry293285@gmail.com>
2021-08-01 01:21:49 +0800

    modify imread arguments for read in convenience and remove original imgs folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <opencv2\opencv.hpp>
-/*---------------------------------------------------------------------*/
 #include "ColorAberrationCorrection.h"
 
 int main(int argc, char *argv[]) {
 
-
-	cv::Mat src = cv::imread(".\\imgs\\original.bmp", cv::IMREAD_COLOR);
+	cv::Mat src = cv::imread(argv[1], cv::IMREAD_COLOR);
 	cv::Mat dst;
 
 	CACorrection(src, dst);
